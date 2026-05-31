fb7575ef4fbaa9a8027e3f6c40b433d36934a398
snodir <nodirbeksadullayev512@outlook.com>
2026-05-31 11:12:41 +0500

    Created DB object dependency sort etl_gas_db_object_dependency_sort in Test env

--- GrubbrrSQL/grubbrr_etl_redesign.sql
+++ GrubbrrSQL/grubbrr_etl_redesign.sql
@@ -7,3 +7,18 @@ SELECT *
 FROM dim.businessdate
 ORDER BY dateid DESC 
 LIMIT 100
+
+WITH unique_records AS (
+SELECT *, ROW_NUMBER() OVER(PARTITION BY table_schema, table_name ORDER BY table_schema, table_name) as rn
+FROM etl.gas_db_object_dependency_sort
+WHERE 1=1
+  --AND table_name NOT IN ('userlocation','device','weather','stage_watermark','peripheral','prod_to_stage_migration_audit','company','datedim','weather','menuentities','menuentities_0912','holidays','prod_to_stage_migration_audit_bkp','location_statistics','duplicate_items_master','item_modifier_group_modifier_mapping','userbehaviour','deviceevent')
+  --AND sysupdatetime_after_migration is NULL ,'userbehaviour','deviceevent','userlocation',
+  --AND table_name IN ('occasionsurvey','userbehaviour','deviceevent')
+)
+SELECT *
+FROM unique_records
+WHERE rn = 1
+ORDER BY dependency_level, 
+         dependency_count, 
+         table_schema, table_name;
